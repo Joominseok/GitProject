f712e5f30ab83cedb730855eccd07d2f1cef1448
Joominseok <alstjr5911@naver.com>
2018-04-13 14:09:00 +0900

    button을 멤버변수로 올렸습니다.

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -8,15 +8,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     
+    private TextView textView;
 
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
     }
 }
